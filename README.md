60783df25b642bc81929c274ef831876a932ed4c
John Gualteros <gualterosjohn40@gmail.com>
2022-12-06 12:32:32 -0500

    Final Commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
+![img](./react-monaco.png)
 # Getting Started with Create React App
-
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import Editor from "@monaco-editor/react";
+import { useRef, useState } from "react";
+import styled from "styled-components";
+import { ResizableBox } from "react-resizable";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const editorRef = useRef(null);
+
+	const [state, setState] = useState({
+		width: 500,
+		height: 200,
+	});
+
+	function handleEditorDidMount(editor, monaco) {
+		editorRef.current = editor;
+	}
+
+	const onResize = (event, { element, size, handle }) => {
+		setState({ width: size.width, height: size.height });
+	};
+
+	function handleEditorChange(value, event) {
+		const renderer = document.getElementById("renderer");
+
+		renderer.innerHTML = value;
+	}
+
+	const Container = styled.div`
+		width: 100%;
+		height: 100vh;
+		display: grid;
+		grid-template-columns: 50% 50%;
+		grid-template-rows: 1fr;
+		padding: 0;
+		overflow: hidden;
+	`;
+
+	const defaultValueEditor = `
+<div class="p-4">
+   <div style="color: red; text-align: center;" role="alert">
+      Alert
+   </div>
+
+   <div class="card">
+      <div class="card-body">
+         <h5 class="card-title">Special title treatment</h5>
+         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+         <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+   </div>
+</div>
+`;
+
+	const RendererHtmlCode = styled.div`
+		width: 100%;
+		height: 100%;
+		background-color: #ff3232;
+		color: #fff;
+		padding: 1rem;
+		overflow: auto;
+	`;
+
+	return (
+		<Container>
+			<Editor
+				height="100vh"
+				defaultLanguage="html"
+				theme="vs-dark"
+				defaultValue={defaultValueEditor}
+				onMount={handleEditorDidMount}
+				onChange={handleEditorChange}
+			/>
+			<ResizableBox width={'100%'} height={200}>
+				<RendererHtmlCode id="renderer"></RendererHtmlCode>
+			</ResizableBox>
+		</Container>
+	);
 }
 
 export default App;
